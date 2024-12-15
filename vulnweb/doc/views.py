b5753fd70868a4995b0ba0e2708ef47ab63cddc2
Hoang Viet Anh <vietanh092004@gmail.com>
2024-12-15 22:15:51 +0700

    Add count vote

--- vulnweb/doc/views.py
+++ vulnweb/doc/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.db.models import F
+from django.urls import reverse
 # Create your views here.
+
 
 def index(request):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -27,9 +30,17 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "doc/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'doc/detail.html', {'question': question, 'error_message': 'Error'},)
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('doc:results', args=(question.id,)))
