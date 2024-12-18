69a0f30db1e9ac01bc4c615c5dc49ec871a6f0d0
Hoang Viet Anh <vietanh092004@gmail.com>
2024-12-18 15:54:01 +0700

    Add learn models

--- vulnweb/doc/models.py
+++ vulnweb/doc/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -19,3 +20,77 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+    
+class Musician(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_starts = models.IntegerField()
+
+
+class Person(models.Model):
+    SHIRT_SIZES = {
+        'S': 'Small',
+        'M': 'Medium',
+        'L': 'Large'
+    }
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    pub_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.headline
+    
+
+# class Book(models.Model):
+#     title = models.TextField(max_length=200)
+
+#     @classmethod
+#     def create(self, title):
+#         book = self(title=title)
+#         return book
+
+class BookManager(models.Manager):
+    def create_book(self, title):
+        book = self.create(title=title)
+        return book
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    objects = BookManager()
+
+
